1ff9c133006b2e2d81fe3ff01f7adfbcec372b57
phoshp <emre.yavuz.842@gmail.com>
2023-12-09 15:50:55 +0300

    day 3 without stb dep

--- day03/day03.c
+++ day03/day03.c
@@ -1,10 +1,48 @@
 #include <ctype.h>
+#include <memory.h>
+#include <printf.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define STB_DS_IMPLEMENTATION
-#include "stb_ds.h"
+void vec_push(void **ptr, void *value, size_t size) {
+  if (*ptr == NULL) {
+    int *data = malloc(2 * (sizeof(int) + size));
+    if (data == NULL) {
+      printf("out of memory");
+      exit(1);
+    }
+
+    data[0] = 1; // length
+    data[1] = 2; // capacity
+    memcpy(data + 2, value, size);
+    *ptr = data + 2;
+  } else {
+    int *data = (int *)*ptr - 2;
+    int len = data[0];
+    int cap = data[1];
+
+    if (len >= cap) {
+      cap *= 2;
+      data = realloc(data, cap * size + sizeof(int) * 2);
+      data[1] = cap;
+      *ptr = data + 2;
+    }
+    memcpy((char *)(data + 2) + len * size, value, size);
+    data[0] = len + 1;
+  }
+}
+
+int vec_len(int *ptr) {
+  int *data = ptr - 2;
+  return data[0];
+}
+
+void vec_free(int *ptr) { free(ptr - 2); }
+
+#define vec_push(ptr, x) vec_push((void **)&ptr, (void *)&x, sizeof(x))
+#define vec_len(x) vec_len((int *)x)
+#define vec_free(x) vec_free((int *)x)
 
 struct part {
   int start;
@@ -19,13 +57,14 @@ struct symbol {
 };
 
 int main(void) {
-  FILE *file = fopen("../data.txt", "r"); // assumes binary file is in the build dir
+  // assumes binary file is in the build dir
+  FILE *file = fopen("../data.txt", "r");
   if (file == NULL) {
     printf("where is the fcking data\n");
     return 1;
   }
 
-  char *line = NULL;
+  char *line;
   size_t len = 0;
 
   struct part *parts;
@@ -38,7 +77,7 @@ int main(void) {
       char c = line[i];
       if (ispunct(c) && c != '.') {
         struct symbol a = {acc + i, acc + i, c};
-        arrpush(symbols, a);
+        vec_push(symbols, a);
       } else if (isdigit(c)) {
         int end = i;
         for (int j = 1; j < (int)len - i; ++j) {
@@ -51,7 +90,7 @@ int main(void) {
         memcpy(str, &line[i], end - i + 1);
 
         struct part p = {acc + i, acc + end, strtol(str, NULL, 10)};
-        arrpush(parts, p);
+        vec_push(parts, p);
         i += end - i;
       }
     }
@@ -66,9 +105,9 @@ int main(void) {
   int ls = (int)len;
   int sumA = 0;
 
-  for (int i = 0; i < arrlen(parts); ++i) {
+  for (int i = 0; i < vec_len(parts); ++i) {
     struct part p = parts[i];
-    for (int j = 0; j < arrlen(symbols); ++j) {
+    for (int j = 0; j < vec_len(symbols); ++j) {
       struct symbol s = symbols[j];
       int dend = abs(s.start - p.end);
       int dstart = abs(s.start - p.start);
@@ -86,13 +125,13 @@ int main(void) {
 
   int sumB = 0;
 
-  for (int i = 0; i < arrlen(symbols); ++i) {
+  for (int i = 0; i < vec_len(symbols); ++i) {
     struct symbol s = symbols[i];
     if (s.sym != '*')
       continue;
     int temp = 0;
 
-    for (int j = 0; j < arrlen(parts); ++j) {
+    for (int j = 0; j < vec_len(parts); ++j) {
       struct part p = parts[j];
       int dend = abs(s.start - p.end);
       int dstart = abs(s.start - p.start);
@@ -115,8 +154,8 @@ int main(void) {
 
   printf("Part B: %d\n", sumB);
 
-  arrfree(parts);
-  arrfree(symbols);
+  vec_free(parts);
+  vec_free(symbols);
 
   return 0;
 }
