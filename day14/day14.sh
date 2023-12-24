3ce9fbb0eb1b6de518ea2136641702830d8c9ba2
phoshp <emre.yavuz.842@gmail.com>
2023-12-24 15:14:24 +0300

    fix day14

--- day14/day14.sh
+++ day14/day14.sh
@@ -3,103 +3,96 @@
 input="./data.txt"
 lines=()
 orig=()
-len=0
+leny=0
+lenx=0
 while read -r line; do
     lines+=("$line")
     orig+=("$line")
-    len=$((len + 1))
+    leny=$((leny + 1))
+    lenx=${#line}
 done < "$input"
 
+nsb=0
 north() {
-    local next=0
-    for ((k=i-1; k>=0; k--)) do
-        n=${lines[k]:j:1}
-        if [[ "$n" == "#" || "$n" == "O" ]]; then
-            next=$((k + 1))
-            break
-        fi
+    for ((j=0; j<lenx; j++)); do
+        next=0
+        for ((i=0; i<leny; i++)); do
+            v=${lines[i]:j:1}
+            if [[ "$v" == "O" ]]; then
+                if [[ "$i" -gt $next ]]; then
+                    lines[i]="${lines[i]:0:j}.${lines[i]:j+1}"
+                    lines[next]="${lines[next]:0:j}O${lines[next]:j+1}"
+                fi
+                next=$((next + 1))
+            elif [[ "$v" == "#" ]]; then
+                next=$((i + 1))
+            fi
+        done
     done
-    lines[i]="${lines[i]:0:j}.${lines[i]:j+1}"
-    lines[next]="${lines[next]:0:j}O${lines[next]:j+1}"
-}
-south() {
-    local next=$((len - 1))
-    for ((k=i+1; k<len; k++)) do
-        n=${lines[k]:j:1}
-        if [[ "$n" == "#" || "$n" == "O" ]]; then
-            next=$((k - 1))
-            break
-        fi
-    done
-    lines[i]="${lines[i]:0:j}.${lines[i]:j+1}"
-    lines[next]="${lines[next]:0:j}O${lines[next]:j+1}"
 }
 west() {
-    local next=0
-    for ((k=j-1; k>=0; k--)) do
-        n=${lines[i]:k:1}
-        if [[ "$n" == "#" || "$n" == "O" ]]; then
-            next=$((k + 1))
-            break
-        fi
+    for ((i=0; i<leny; i++)); do
+        next=0
+        for ((j=0; j<lenx; j++)); do
+            v=${lines[i]:j:1}
+            if [[ "$v" == "O" ]]; then
+                if [[ "$j" -gt $next ]]; then
+                    lines[i]="${lines[i]:0:j}.${lines[i]:j+1}"
+                    lines[i]="${lines[i]:0:next}O${lines[i]:next+1}"
+                fi
+                next=$((next + 1))
+            elif [[ "$v" == "#" ]]; then
+                next=$((j + 1))
+            fi
+        done
     done
-    lines[i]="${lines[i]:0:j}.${lines[i]:j+1}"
-    lines[i]="${lines[i]:0:next}O${lines[i]:next+1}"
+}
+south() {
+    for ((j=0; j<lenx; j++)); do
+        next=$((leny - 1))
+        for ((i=leny - 1; i>=0; i--)); do
+            v=${lines[i]:j:1}
+            if [[ "$v" == "O" ]]; then
+                if [[ "$i" -lt $next ]]; then
+                    lines[i]="${lines[i]:0:j}.${lines[i]:j+1}"
+                    lines[next]="${lines[next]:0:j}O${lines[next]:j+1}"
+                fi
+                next=$((next - 1))
+            elif [[ "$v" == "#" ]]; then
+                next=$((i - 1))
+            fi
+        done
+    done
 }
 east() {
-    local next=$((len - 1))
-    for ((k=j+1; k<len; k++)) do
-        n=${lines[i]:k:1}
-        if [[ "$n" == "#" || "$n" == "O" ]]; then
-            next=$((k - 1))
-            break
-        fi
-    done
-    lines[i]="${lines[i]:0:j}.${lines[i]:j+1}"
-    lines[i]="${lines[i]:0:next}O${lines[i]:next+1}"
-}
-
-turn() {
-    local turn=$1
-    for i in "${!lines[@]}"; do
-        if [[ "$turn" == 2 ]]; then
-            i=$((len - i - 1))
-        fi
-        for j in "${!lines[@]}"; do
-            if [[ "$turn" == 4 ]]; then
-                j=$((len - j - 1))
-            fi
+    for ((i=0; i<leny; i++)); do
+        next=$((lenx - 1))
+        for ((j=lenx - 1; j>=0; j--)); do
             v=${lines[i]:j:1}
             if [[ "$v" == "O" ]]; then
-                if [[ "$turn" == 1 ]]; then
-                    north
-                elif [[ "$turn" == 2 ]]; then
-                    south
-                elif [[ "$turn" == 3 ]]; then
-                    west
-                elif [[ "$turn" == 4 ]]; then
-                    east
+                if [[ "$j" -lt $next ]]; then
+                    lines[i]="${lines[i]:0:j}.${lines[i]:j+1}"
+                    lines[i]="${lines[i]:0:next}O${lines[i]:next+1}"
                 fi
+                nsb=$((nsb + leny - i))
+                next=$((next - 1))
+            elif [[ "$v" == "#" ]]; then
+                next=$((j - 1))
             fi
         done
     done
 }
 
+north
 load=0
-check_load() {
-    load=0
-    for i in "${!lines[@]}"; do
-        for j in "${!lines[@]}"; do
-            v=${lines[i]:j:1}
-            if [[ "$v" == "O" ]]; then
-                load=$((load + len - i))
-            fi
-        done
+for ((i=0; i<leny; i++)); do
+    for ((j=0; j<lenx; j++)); do
+        v=${lines[i]:j:1}
+        if [[ "$v" == "O" ]]; then
+            load=$((load + leny - i))
+        fi
     done
-}
-
-turn 1
-check_load
+done
 echo "Part A: $load" 
 
 lines=("${orig[@]}")
@@ -109,19 +102,19 @@ declare -A history
 cycle_start=0
 cycle_len=0
 
-for ((m=0; m<300; m++)) do
-    turn 1
-    turn 3
-    turn 2
-    turn 4
-    check_load
-    loads+=("$load")
+for ((m=0; m<1000000000; m++)); do
+    nsb=0
+    north
+    west
+    south
+    east
+    loads+=("$nsb")
 
-    if [[ $m -gt 20 ]]; then
-        hlen=${#loads}
+    if [[ "$m" -gt 20 ]]; then
+        hlen=${#loads[@]}
         state="${loads[*]:hlen-20}"
         if [[ -v history[$state] ]]; then
-            cycle_start=${history[state]}
+            cycle_start=${history[$state]}
             cycle_len=$((m - cycle_start))
             break
         fi
@@ -130,8 +123,6 @@ for ((m=0; m<300; m++)) do
 done
 
 target=$((1000000000 - cycle_start))
-offset=$((target % cycle_len - 1))
+offset=$((target % cycle_len))
 
-echo "Part B: ${loads[$cycle_start + $offset]}"
-
-
+echo "Part B: ${loads[cycle_start + offset - 1]}"
