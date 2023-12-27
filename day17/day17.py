08e66a7dbfde85445501093c4a7fa10beda29771
phoshp <emre.yavuz.842@gmail.com>
2023-12-27 23:43:31 +0300

    ololo

--- day17/day17.py
+++ day17/day17.py
@@ -19,9 +19,9 @@ with open("data.txt") as f:
             nx, ny = x + ndx, y + ndy
             straight = (dx == ndx and dy == ndy)
 
-            if (ndx == -dx and ndy == -dy) or (straight and dc == 2) or nx < 0 or ny < 0 or nx == m or ny == n:
+            if (ndx == -dx and ndy == -dy) or (straight and dc == 3) or nx < 0 or ny < 0 or nx == m or ny == n:
                 continue
-            heapq.heappush(queue, (loss + grid[nx][ny], nx, ny, ndx, ndy, dc + 1 if straight else 0))
+            heapq.heappush(queue, (loss + grid[nx][ny], nx, ny, ndx, ndy, dc + 1 if straight else 1))
     print("Part 1:", loss)
 
     queue = [(0, 0, 0, 0, 0, 0)]
@@ -29,7 +29,7 @@ with open("data.txt") as f:
     while queue:
         loss, x, y, dx, dy, dc = heapq.heappop(queue)
         if x == m-1 and y == n-1:
-            if dc < 3:
+            if dc < 4:
                 continue
             break
         if (x, y, dx, dy, dc) in closed:
@@ -39,7 +39,7 @@ with open("data.txt") as f:
             nx, ny = x + ndx, y + ndy
             straight = (dx == ndx and dy == ndy) or (dx == 0 and dy == 0)
 
-            if dc < 3 and not straight or (ndx == -dx and ndy == -dy) or (straight and dc == 9) or nx < 0 or ny < 0 or nx == m or ny == n:
+            if (dc < 4 and not straight) or (ndx == -dx and ndy == -dy) or (straight and dc == 10) or nx < 0 or ny < 0 or nx == m or ny == n:
                 continue
-            heapq.heappush(queue, (loss + grid[nx][ny], nx, ny, ndx, ndy, dc + 1 if straight else 0))
+            heapq.heappush(queue, (loss + grid[nx][ny], nx, ny, ndx, ndy, dc + 1 if straight else 1))
     print("Part 2:", loss)
